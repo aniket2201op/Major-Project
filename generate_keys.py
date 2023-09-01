843001b903d6615b19d8b6dbc1b7a07fc7d55208
Aniket Shinde <aniket.shinde.co@gmail.com>
2023-09-01 15:03:00 +0530

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,11 +3,16 @@ from pathlib import Path
 
 import streamlit_authenticator as stauth
 
-names = ["Peter Parker"]
-usernames = ["pparker"]
-passwords = ["abc123"]
-
-hashed_passwords = stauth.Hasher(passwords).generate()
+credentials = {
+        "usernames":{
+            "jsmith92":{
+                "name":"john smith",
+                "password":"abc123"
+                }
+                     
+            }
+        }
+hashed_passwords = stauth.Hasher(credentials['usernames']['jsmith92']['password']).generate()
 
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file:
