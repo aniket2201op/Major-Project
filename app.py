71e1052e6173502a154e3638933012161e66ae8e
Aniket Shinde <114918014+aniket2201op@users.noreply.github.com>
2023-09-01 08:52:09 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import pickle
+from pathlib import Path
+
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+
+# --- USER AUTHENTICATION ---
+# names = ["Peter Parker", "Rebecca Miller"]
+# usernames = ["pparker", "rmiller"]
+credentials = {
+        "usernames":{
+            "jsmith92":{
+                "name":"john smith",
+                "password":"$2b$12$HUxcAwtMpfPNS60/g190cujUgbTVD9Xl0ai65Oy7k7Kt8GhbxtsUe�a"
+                }
+                     
+            }
+        }
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(credentials,
+    "sales_dashboard", "abcdef", cookie_expiry_days=0)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    # ---- READ EXCEL ----
+    st.write("Hello World")
